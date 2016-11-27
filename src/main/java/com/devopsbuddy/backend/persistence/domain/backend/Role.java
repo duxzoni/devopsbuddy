890a673429ad061f16689f6f166bf980b05a3c6e
Eduardo Ghizoni <duxzoni@gmail.com>
2016-11-27 21:09:16 -0200

    Created the JPOA entity model for Spring Security

---
+++ src/main/java/com/devopsbuddy/backend/persistence/domain/backend/Role.java
@@ -0,0 +1,62 @@
+package com.devopsbuddy.backend.persistence.domain.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public Role(){
+    }
+
+    @Id
+    private int id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<UserRole> userRole;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserRole> getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Set<UserRole> userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return id == role.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
